d4842372a9b2de496800ce95157dcc3161147a8f
Alexander Slepchenko <asander@list.ru>
2020-10-28 21:44:08 +0300

    add check update()

--- src/com/urise/webapp/MainTestArrayStorage.java
+++ src/com/urise/webapp/MainTestArrayStorage.java
@@ -1,13 +1,14 @@
 package com.urise.webapp;
 
 import com.urise.webapp.model.Resume;
+import com.urise.webapp.storage.AbstractArrayStorage;
 import com.urise.webapp.storage.ArrayStorage;
 
 /**
  * Test for your com.urise.webapp.storage.ArrayStorage implementation
  */
 public class MainTestArrayStorage {
-    static final ArrayStorage ARRAY_STORAGE = new ArrayStorage();
+    static final AbstractArrayStorage ARRAY_STORAGE = new ArrayStorage();
 
     public static void main(String[] args) {
         Resume r1 = new Resume();
@@ -28,6 +29,9 @@ public class MainTestArrayStorage {
         System.out.println("Size: " + ARRAY_STORAGE.size());
 
         System.out.println("Get dummy: " + ARRAY_STORAGE.get("dummy"));
+        printAll();
+        ARRAY_STORAGE.update(r3);
+        r3.setUuid(r3.toString() + " updated");
 
         printAll();
         ARRAY_STORAGE.delete(r1.getUuid());
