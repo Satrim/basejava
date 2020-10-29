85d7771542154a68a8a15b0f54f125a7d1aaaeb2
Satrim <59724286+Satrim@users.noreply.github.com>
2020-10-29 16:58:48 +0300

    remove methods without subclasses

--- src/com/urise/webapp/storage/AbstractArrayStorage.java
+++ src/com/urise/webapp/storage/AbstractArrayStorage.java
@@ -10,6 +10,34 @@ abstract public class AbstractArrayStorage implements Storage {
     protected Resume[] storage = new Resume[STORAGE_LIMIT];
     protected int size = 0;
 
+    public void delete(String uuid) {
+        int index = getIndex(uuid);
+        if (index > -1) {
+            if (index == STORAGE_LIMIT - 1) {
+                storage[STORAGE_LIMIT - 1] = null;
+                size--;
+                return;
+            }
+
+            for (int i = index + 1; i < size; i++) {
+                storage[i - 1] = storage[i];
+            }
+            size--;
+        } else {
+            System.out.println("Резюме " + uuid + " не найдено");
+        }
+    }
+
+    public void update(Resume resume) {
+        int index = getIndex(resume.getUuid());
+        if (index > -1) {
+            storage[index] = resume;
+            System.out.println("Резюме " + storage[index].getUuid() + " обновлено");
+        } else {
+            System.out.println("Резюме с именем " + resume.getUuid() + " не найдено");
+        }
+    }
+
     public void clear() {
         Arrays.fill(storage, 0, size, null);
         size = 0;
@@ -25,29 +53,14 @@ abstract public class AbstractArrayStorage implements Storage {
 
     public Resume get(String uuid) {
         int index = getIndex(uuid);
-        if (index == -1) {
+        if (index < 0) {
             System.out.println("Резюме " + uuid + " не существует");
             return null;
         }
         return storage[index];
     }
 
-
-
-//    public abstract void clear();
-
-    public abstract void update(Resume r);
-
     public abstract void save(Resume r);
 
-    public abstract void delete(String uuid);
-
-//    public abstract Resume[] getAll();
-
     protected abstract int getIndex(String uuid);
-
-    // Не разобрался, что с этим делать
-
-
-
 }
