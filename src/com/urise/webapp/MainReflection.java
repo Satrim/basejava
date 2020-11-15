c30588240ef1a646265482a3e77220da1bbb9a30
Alexander Slepchenko <asander@list.ru>
2020-11-15 21:23:58 +0300

    learn reflection

--- src/com/urise/webapp/MainReflection.java
+++ src/com/urise/webapp/MainReflection.java
@@ -1,13 +1,15 @@
 package com.urise.webapp;
 
 import com.urise.webapp.model.Resume;
+import com.urise.webapp.storage.AbstractArrayStorage;
+import com.urise.webapp.storage.ArrayStorage;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 public class MainReflection {
-    public static void main(String[] args) throws IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+    public static void main(String[] args) throws IllegalAccessException, NoSuchMethodException, InvocationTargetException, NoSuchFieldException {
         Resume r = new Resume();
         Field[] field = r.getClass().getDeclaredFields();
         field[0].setAccessible(true);
@@ -32,6 +34,15 @@ public class MainReflection {
 
         System.out.println();
         System.out.println(r);
+
+        AbstractArrayStorage storage = new ArrayStorage();
+        Class cs = AbstractArrayStorage.class;
+        Field[] fields = cs.getDeclaredFields();
+
+        System.out.println("_______________");
+
+        fields[1].setAccessible(true);
+
     }
 
 }
