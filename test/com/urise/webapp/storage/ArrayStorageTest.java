3b2025dc690f0b417579e470dc856460d6a9ea0c
Satrim <59724286+Satrim@users.noreply.github.com>
2020-12-07 20:53:21 +0300

    added @override methods into constructor

--- test/com/urise/webapp/storage/ArrayStorageTest.java
+++ test/com/urise/webapp/storage/ArrayStorageTest.java
@@ -1,7 +1,29 @@
 package com.urise.webapp.storage;
 
+import com.urise.webapp.model.Resume;
+
 public class ArrayStorageTest extends AbstractArrayStorageTest {
     public ArrayStorageTest() {
-        super(new ArrayStorage());
+        super(new ArrayStorage() {
+            @Override
+            public void saveResume(Resume resume, int index) {
+
+            }
+
+            @Override
+            public void deleteResume(int index) {
+
+            }
+
+            @Override
+            public void updateResume(Resume resume, int index) {
+
+            }
+
+            @Override
+            public Resume getResume(int index) {
+                return null;
+            }
+        });
     }
 }
--- test/com/urise/webapp/storage/SortedArrayStorageTest.java
+++ test/com/urise/webapp/storage/SortedArrayStorageTest.java
@@ -1,7 +1,29 @@
 package com.urise.webapp.storage;
 
+import com.urise.webapp.model.Resume;
+
 public class SortedArrayStorageTest extends AbstractArrayStorageTest {
     public SortedArrayStorageTest() {
-        super(new SortedArrayStorage());
+        super(new SortedArrayStorage() {
+            @Override
+            public void saveResume(Resume resume, int index) {
+
+            }
+
+            @Override
+            public void deleteResume(int index) {
+
+            }
+
+            @Override
+            public void updateResume(Resume resume, int index) {
+
+            }
+
+            @Override
+            public Resume getResume(int index) {
+                return null;
+            }
+        });
     }
 }
