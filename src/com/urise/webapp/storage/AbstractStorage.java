41741fcad38a7618d698045d74cbaf74e2ef7252
Satrim <59724286+Satrim@users.noreply.github.com>
2020-12-10 14:27:23 +0300

    added check for null

--- src/com/urise/webapp/storage/AbstractStorage.java
+++ src/com/urise/webapp/storage/AbstractStorage.java
@@ -2,11 +2,15 @@ package com.urise.webapp.storage;
 
 import com.urise.webapp.exception.ExistStorageException;
 import com.urise.webapp.exception.NotExistStorageException;
+import com.urise.webapp.exception.StorageException;
 import com.urise.webapp.model.Resume;
 
 public abstract class AbstractStorage implements Storage {
 
     public void save(Resume resume) {
+        if (resume.getUuid() == null) {
+            throw new StorageException("Имя задано некоректно", resume.getUuid());
+        }
         int index = getIndex(resume.getUuid());
         if (index > -1) {
             throw new ExistStorageException(resume.getUuid());
@@ -16,6 +20,9 @@ public abstract class AbstractStorage implements Storage {
     }
 
     public void delete(String uuid) {
+        if (uuid == null) {
+            throw new StorageException("Имя задано некоректно", uuid);
+        }
         int index = getIndex(uuid);
         if (index > -1) {
             deleteResume(index);
@@ -25,6 +32,9 @@ public abstract class AbstractStorage implements Storage {
     }
 
     public void update(Resume resume) {
+        if (resume.getUuid() == null) {
+            throw new StorageException("Имя задано некоректно", resume.getUuid());
+        }
         int index = getIndex(resume.getUuid());
         if (index > -1) {
             updateResume(resume, index);
@@ -35,6 +45,9 @@ public abstract class AbstractStorage implements Storage {
     }
 
     public Resume get(String uuid) {
+        if (uuid == null) {
+            throw new StorageException("Имя задано некоректно", uuid);
+        }
         int index = getIndex(uuid);
         if (index > -1) {
             return getResume(index);
