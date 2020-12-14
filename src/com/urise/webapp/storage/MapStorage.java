ba275743c9d349c8a0fbdec8416def93826496cb
Satrim <59724286+Satrim@users.noreply.github.com>
2020-12-14 14:54:44 +0300

    deleted

--- src/com/urise/webapp/storage/MapStorage.java
+++ src/com/urise/webapp/storage/MapStorage.java
@@ -8,73 +8,31 @@ import java.util.Map;
 
 public class MapStorage extends AbstractStorage {
     private static Map<String, Resume> storage = new HashMap<>();
-    private byte[] indexID = new byte[10_000];
 
-    private int addID() {
-        for (int i = 0; i < indexID.length; i++) {
-            if (indexID[i] == 0) {
-                indexID[i] = 1;
-                return i;
-            }
-        }
-        return -1;
-    }
+     public int getKey(String uuid) {
 
-    private void delID(int id) {
-        for (int i = 0; i < indexID.length; i++) {
-            if (id == i) {
-                indexID[i] = 0;
-            }
-        }
-    }
-
-    public int getIndex(String uuid) {
-        for (Map.Entry<String, Resume> pair : storage.entrySet()) {
-            if (pair.getKey().equals(uuid)) {
-                return pair.getValue().getID();
-            }
-        }
         return -1;
     }
 
     public void saveResume(Resume resume, int index) {
-        resume.setID(addID());
-        storage.put(resume.getUuid(), resume);
+
     }
 
     public void deleteResume(int index) {
-        for (Map.Entry<String, Resume> pair : storage.entrySet()) {
-            if (pair.getValue().getID() == index) {
-                storage.remove(pair.getKey());
-                delID(index);
-                break;
-            }
-        }
+
     }
 
     public void updateResume(Resume resume, int index) {
-        for (Map.Entry<String, Resume> pair : storage.entrySet()) {
-            if (resume.getUuid().equals(pair.getKey())) {
-                Resume updateResume = new Resume(resume.getUuid());
-                updateResume.setID(index);
-                storage.put(resume.getUuid(), updateResume);
-                break;
-            }
-        }
+
     }
 
     public Resume getResume(int index) {
-        for (Map.Entry<String, Resume> pair : storage.entrySet()) {
-            if (pair.getValue().getID() == index) {
-                return pair.getValue();
-            }
-        }
+
         return null;
     }
 
     public void clear() {
-        storage.clear();
-        indexID = new byte[10_000];
+
     }
 
     public Resume[] getAll() {
